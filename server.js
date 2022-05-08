3d20c066daf8aaef28270c27fa702d95438480f7
Lucca Araujo <lucca.ara7@gmail.com>
2022-05-08 13:38:51 -0700

    add a function to add notes and a post route

--- server.js
+++ server.js
@@ -11,17 +11,42 @@ app.use(express.json());
 
 
 // Routes
+
+// Data from the notes in json
 app.get("/api/notes", (req, res) => {
-    res.send('Test');
-});
+    res.json(notesData)
+})
 
 // Html routes
-app.get("/", (req, res) =>{
+app.get("*", (req, res) =>{
     res.sendFile(path.join(__dirname, "/Develop/public/index.html"))
 });
 
 app.get("/notes", (req, res)=>{
     res.sendFile(path.join(__dirname, "/Develop/public/notes.html"))
+});
+
+const addNotes = function(request, notesUser){
+    const jsonFile = path.join(__dirname, "/Develop/db/db.json");
+    const postNote = request;
+    request.id = notesUser[0];
+    notesUser[0]++;
+    notesUser.push(postNote);
+
+    fs.writeFile( jsonFile, JSON.stringify(notesUser), (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Your note has been successfully saved!");
+    });
+    return postNote;
+
+};
+
+// Post the data from addNotes
+app.post("/api/notes", (req, res) => {
+    const saveNotes = addNotes(req.body, notesData)
+    res.json(saveNotes)
 });
 
 
